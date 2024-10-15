5a50edcb0cd6092cbdaed68c8c3b0dc79363bbd9
ezralee0010 <122120949+ezralee0010@users.noreply.github.com>
2024-10-15 22:56:55 +0000

    Added

---
+++ howdy.rb
@@ -0,0 +1,16 @@
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+
+require "active_support/all"
+
+pp Date.today
+
+# Write a program that asks the user for their name
+# Then say, "Hello, <name>!"
+
+pp "howdy what's your name"
+
+x = gets
+pp "Gotcha #{x}!"
